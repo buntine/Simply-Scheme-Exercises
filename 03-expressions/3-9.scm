5558976c849f8d1e60af4afa8b471eacbdcb0a04
Andrew Buntine <perunwit@perunwit-laptop.(none)>
2009-12-18 23:53:30 +1100

    Chapter 3 complete

---
+++ 03-expressions/3-9.scm
@@ -0,0 +1,19 @@
+; The expression (+ 8 2) has the value 10. It is a compound expression made up of
+; three atoms. For this problem, write five other Scheme expressions whose values are also
+; the number ten:
+;
+; - An atom (0)
+; - Another compound expression made up of three atoms (1)
+; - A compound expression made up of four atoms (2)
+; - A compound expression made up of an atom and two compound subexpressions (3)
+; - Any other kind of expression (4)
+
+10                    ; 0
+
+(- 15 5)              ; 1
+
+(+ 1 8 1)             ; 2
+
+(- (+ 10 4) (* 4 1))  ; 3
+
+(sqrt 100)            ; 4
