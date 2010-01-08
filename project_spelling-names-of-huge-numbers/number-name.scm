f0363d9f2eb7cb43be2ec698cbfce532c801c061
Andrew Buntine <perunwit@perunwit-laptop.(none)>
2010-01-09 09:34:42 +1100

    Continued on project #2

--- project_spelling-names-of-huge-numbers/number-name.scm
+++ project_spelling-names-of-huge-numbers/number-name.scm
@@ -22,19 +22,19 @@
 (define (print-quantities n)
   n)
 
-(define (print-magnitude nums m)
-  (if (not (number? (first (bf nums))))
-    nums
-    (let ((onlynums (keep (lambda (n) (number? n)) nums))
-          (increment (/ (+ m 1) 2)))
-      (print-magnitude
-        (se (first-n onlynums (- (count onlynums) increment))
-            (item increment quantities)
-            (last-n nums m)) (+ m 2)))))
+(define (print-magnitudes nums m)
+  (let ((onlynums (keep (lambda (n) (number? n)) nums))
+        (increment (/ (+ m 1) 2)))
+    (cond ((not (number? (first (bf nums)))) nums)
+          (else
+            (print-magnitudes
+              (se (first-n onlynums (- (count onlynums) increment))
+                  (item increment quantities)
+                  (last-n nums m)) (+ m 2))))))
 
 (define (number-name n)
   (print-quantities
     (if (< n 999)
       (se n)
-      (print-magnitude
+      (print-magnitudes
         (groups-of-three n) 1))))
