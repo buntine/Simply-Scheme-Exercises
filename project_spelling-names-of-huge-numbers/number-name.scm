3641b2de4a8e17160589eaa1e2a25c5f3fc09437
Andrew Buntine <perunwit@perunwit-laptop.(none)>
2010-01-10 04:59:35 +1100

    Finally got magnitudes inserted correctly into printed number. Checkpoint #1

--- project_spelling-names-of-huge-numbers/number-name.scm
+++ project_spelling-names-of-huge-numbers/number-name.scm
@@ -10,6 +10,8 @@
 (define (first-n wd n)
   ((repeated bl (- (count wd) n)) wd))
 
+; Breaks the supplied word into a sentence of three-word groups.
+; '1222333899 -> '(1 222 333 899)
 (define (groups-of-three sent)
   (cond ((word? sent) (groups-of-three (se sent)))
         ((> (count (first sent)) 3)
@@ -19,22 +21,31 @@
                 (bf sent))))
         (else sent)))
 
+; Translates each number group into an English equivalant.
 (define (print-quantities n)
   n)
 
-(define (print-magnitudes nums m)
-  (let ((onlynums (keep (lambda (n) (number? n)) nums))
-        (increment (/ (+ m 1) 2)))
-    (cond ((not (number? (first (bf nums)))) nums)
-          (else
-            (print-magnitudes
-              (se (first-n onlynums (- (count onlynums) increment))
-                  (item increment quantities)
-                  (last-n nums m)) (+ m 2))))))
+; Inserts the magnitudes for each group of numbers.
+; Any set of three zeroes are ignored.
+(define (print-magnitudes nums m i)
+  (cond ((= (count nums) m)
+          nums)
+        ((= (item (- (count nums) m) nums) "000")
+          (print-magnitudes nums (+ m 1) (+ i 1)))
+        (else
+          (print-magnitudes
+            (se (butlast-n nums m)
+              (item i quantities)
+              (last-n nums m)) (+ m 2) (+ i 1)))))
 
+; Consumes an arbitrary-sized number and presents
+; and English-language version of it.
 (define (number-name n)
-  (print-quantities
-    (if (< n 999)
-      (se n)
-      (print-magnitudes
-        (groups-of-three n) 1))))
+  (let ((nums (groups-of-three n)))
+    (print-quantities
+      (if (< n 999)
+        (se n)
+        (keep
+          (lambda (n) (not (equal? n "000")))
+          (se (print-magnitudes
+            (bl nums) 0 1) (last nums)))))))
