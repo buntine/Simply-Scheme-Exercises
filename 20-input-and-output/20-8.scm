89768d078ef90a1d7a006e9d882789fd050fe1d0
Andrew Buntine <perunwit@perunwit-laptop.(none)>
2010-01-26 18:31:32 +1100

    Added 20.8

---
+++ 20-input-and-output/20-8.scm
@@ -0,0 +1,173 @@
+; At the end of the game, if the computer wins or ties, you never find out which
+; square it chose for its final move. Modify the program to correct this. (Notice that this
+; exercise requires you to make play-ttt-helper non-functional.)
+
+(define (stupid-ttt position letter)
+  (location ' position))
+
+(define (location letter word)
+  (if (equal? letter (first word))
+    1
+    (+ 1 (location letter (bf word)))))
+
+(define (ask-user position letter)
+  (print-position position)
+  (display letter)
+  (display "'s move: ")
+  (read))
+
+(define (print-position position)
+  (print-row (subword position 1 3))
+  (show "-+-+-")
+  (print-row (subword position 4 6))
+  (show "-+-+-")
+  (print-row (subword position 7 9))
+  (newline))
+
+(define (print-row row)
+  (maybe-display (first row))
+  (display "|")
+  (maybe-display (first (bf row)))
+  (display "|")
+  (maybe-display (last row))
+  (newline))
+
+(define (maybe-display letter)
+  (if (not (equal? letter '_))
+      (display letter)
+      (display " ")))
+
+(define (subword wd start end)
+  ((repeated bf (- start 1))
+   ((repeated bl (- (count wd) end))
+    wd)))
+
+(define (already-won? position who)
+  (member? (word who who who) (find-triples position)))
+
+(define (tie-game? position)
+  (not (member? '_ position)))
+
+(define (play-ttt x-strat o-strat)
+  (play-ttt-helper x-strat o-strat '_________ 'x))
+
+(define (add-move square letter position)
+  (if (= square 1)
+    (word letter (bf position))
+    (word (first position)
+          (add-move (- square 1) letter (bf position)))))
+
+(define (play-ttt-helper x-strat o-strat position whose-turn)
+  (cond ((already-won? position (opponent whose-turn))
+          (begin (print-position position)
+                 (list (opponent whose-turn) 'wins!)))
+        ((tie-game? position)
+          (begin (print-position position)
+                 '(tie game)))
+        (else (let ((square (if (equal? whose-turn 'x)
+                              (x-strat position 'x)
+                              (o-strat position 'o))))
+                (play-ttt-helper x-strat
+                                 o-strat
+                                 (add-move square whose-turn position)
+                                 (opponent whose-turn))))))
+
+(define (ttt position me)
+  (ttt-choose (find-triples position) me))
+
+(define (find-triples position)
+  (every (lambda (comb) (substitute-triple comb position))
+         '(123 456 789 147 258 369 159 357)))
+
+(define (substitute-triple combination position)
+  (accumulate word
+              (every (lambda (square)
+                       (substitute-letter square position))
+                     combination)))
+
+(define (substitute-letter square position)
+  (if (equal? '_ (item square position))
+    square
+    (item square position)))
+
+(define (ttt-choose triples me)
+  (cond ((i-can-win? triples me))
+        ((opponent-can-win? triples me))
+        ((i-can-fork? triples me))
+        ((i-can-advance? triples me))
+        (else (best-free-square triples))))
+
+(define (i-can-win? triples me)
+  (choose-win
+   (keep (lambda (triple) (my-pair? triple me))
+         triples)))
+
+(define (my-pair? triple me)
+  (and (= (appearances me triple) 2)
+       (= (appearances (opponent me) triple) 0)))
+
+(define (opponent letter)
+  (if (equal? letter 'x) 'o 'x))
+
+(define (choose-win winning-triples)
+  (if (empty? winning-triples)
+    #f
+    (keep number? (first winning-triples))))
+
+(define (opponent-can-win? triples me)
+  (i-can-win? triples (opponent me)))
+
+(define (i-can-fork? triples me)
+  (first-if-any (pivots triples me)))
+
+(define (first-if-any sent)
+  (if (empty? sent)
+    #f
+    (first sent)))
+
+(define (pivots triples me)
+  (repeated-numbers (keep (lambda (triple) (my-single? triple me))
+                          triples)))
+
+(define (my-single? triple me)
+  (and (= (appearances me triple) 1)
+       (= (appearances (opponent me) triple) 0)))
+
+(define (repeated-numbers sent)
+  (every first
+         (keep (lambda (wd) (>= (count wd) 2))
+               (sort-digits (accumulate word sent)))))
+
+(define (sort-digits number-word)
+  (every (lambda (digit) (extract-digit digit number-word))
+         '(1 2 3 4 5 6 7 8 9)))
+
+(define (extract-digit desired-digit wd)
+  (keep (lambda (wd-digit) (equal? wd-digit desired-digit)) wd))
+
+(define (i-can-advance? triples me)
+  (best-move (keep (lambda (triple) (my-single? triple me)) triples)
+             triples
+             me))
+
+(define (best-move my-triples all-triples me)
+  (if (empty? my-triples)
+    #f
+    (best-square (first my-triples) all-triples me)))
+
+(define (best-square my-triple triples me)
+  (best-square-helper (pivots triples (opponent me))
+                      (keep number? my-triple)))
+
+(define (best-square-helper opponent-pivots pair)
+  (if (member? (first pair) opponent-pivots)
+    (first pair)
+    (last pair)))
+
+(define (best-free-square triples)
+  (first-choice (accumulate word triples)
+                '(5 1 3 7 9 2 4 6 8)))
+
+(define (first-choice possibilities preferences)
+  (first (keep (lambda (square) (member? square possibilities))
+               preferences)))
